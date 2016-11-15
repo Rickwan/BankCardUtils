2467328c347c22b34d2dc074baa7a4e26c0bddaf
MB <MB@Rickwans-Mac-Mini.local>
2016-11-15 13:35:10 +0800

    update

--- app/src/main/java/com/rickwan/bankcard/MainActivity.java
+++ app/src/main/java/com/rickwan/bankcard/MainActivity.java
@@ -23,8 +23,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        intputView= (EditText )findViewById(R.id.banck_card_et);
-        cardView= (TextView )findViewById(R.id.banck_card_tv);
+        intputView = (EditText) findViewById(R.id.banck_card_et);
+        cardView = (TextView) findViewById(R.id.banck_card_tv);
 
         findViewById(R.id.verify_btn).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,29 +37,30 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private void verify(){
+    private void verify() {
 
 //        String cardNumber = "6225881289141234";
 
         String input = intputView.getText().toString();
 
-        BankVerifyUtils.verifyBankCard(this,input, new BankVerifyUtils.BankCardVerifyListener() {
+        BankVerifyUtils.verifyBankCard(this, input, new BankVerifyUtils.BankCardVerifyListener() {
             @Override
             public void verify(final Bank bank) {
 
-                if (bank==null){
-                    cardView.setText("银行卡无效");
-                }else {
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            cardView.setText("银行卡类型:"+bank.card_type+"\n银行卡名称:"+bank.bank_name);
-                        }
-                    });
 
-                }
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (bank == null) {
+                            cardView.setText("银行卡无效");
+                        } else {
+                            cardView.setText("银行卡类型:" + bank.card_type + "\n银行卡名称:" + bank.bank_name);
+                        }
+                    }
+                });
 
             }
+
         });
     }
 }
